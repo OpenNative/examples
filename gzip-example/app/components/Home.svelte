37dcf3650123e1000834aa219e474e7c161611e1
ammarahm-ed <ammarahmed6506@gmail.com>
2023-01-29 09:01:44 +0500

    initial commit

---
+++ gzip-example/app/components/Home.svelte
@@ -0,0 +1,56 @@
+<page>
+    <actionBar title="Gzip example" />
+    <flexboxLayout class="info">
+        <button
+            text="Compress string to base64"
+            class='btn'
+            on:tap={compress}
+        />
+        <label textWrap="true" class="label" text={compressed} />
+
+        <button
+            text="Decompress base64 string"
+            class='btn'
+            on:tap={decompress}
+        />
+        <label class="label" text={decompressed} />
+    </flexboxLayout>
+</page>
+
+<script lang="ts">
+    import { deflate, inflate } from 'react-native-gzip';
+    let compressed = "";
+    let decompressed = "";
+
+    const compress = async () => {
+        compressed = await deflate("NativeScript is awesome");
+    }
+
+    const decompress = async () => {
+        if (compressed) {
+            decompressed = await inflate(compressed);
+        }
+    }
+</script>
+
+<style>
+    .btn {
+        background-color: coral;
+        padding: 0 20;
+        color: white;
+        border-radius: 100;
+        width: 300;
+    }
+    .label {
+        font-size: 16;
+        max-width: 300;
+    }
+    .info {
+        font-size: 20;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        flex-wrap: wrap;
+        padding: 20;
+    }
+</style>
--- gzip-example/nativescript.config.ts
+++ gzip-example/nativescript.config.ts
@@ -1,7 +1,7 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-  id: 'org.nativescript.opennativensexample',
+  id: 'org.nativescript.gzipexample',
   appPath: 'app',
   appResourcesPath: 'App_Resources',
   android: {
