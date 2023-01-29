c7dc3f52c59ad15d5f76ec55e73d521f7b2a08a5
ammarahm-ed <ammarahmed6506@gmail.com>
2023-01-29 11:11:16 +0500

    add deviceinfo-example

---
+++ device-info-example/app/components/Home.vue
@@ -0,0 +1,52 @@
+<template>
+    <Page>
+        <ActionBar>
+            <Label text="DeviceInfo example"/>
+        </ActionBar>
+        <FlexboxLayout class="container">
+            <Label class="info" :text="name"/>
+
+            <Label class="info" :text="version"/>
+
+            <Label class="info" :text="carrier"/>
+        </FlexboxLayout>
+    </Page>
+</template>
+
+<script>
+  import { getApplicationName, getVersion, getCarrierSync } from "react-native-device-info";
+  export default {
+    computed: {
+      name() {
+        return "App name:" + getApplicationName();
+      },
+      version() {
+        return "App version:" + getVersion();
+      },
+      carrier() {
+        return "Carrier:" + getCarrierSync()
+      }
+    }
+  };
+</script>
+
+<style scoped lang="scss">
+    @import '@nativescript/theme/scss/variables/blue';
+
+    // Custom styles
+    .fas {
+        @include colorize($color: accent);
+    }
+
+    .info {
+        font-size: 20;
+        margin-bottom: 10;
+    }
+
+    .container {
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+    }
+</style>
